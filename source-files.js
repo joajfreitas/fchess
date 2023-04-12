f933fc8b1618ab409f6ef93e1a95f01a91a5006e
joajfreitas <joajfreitas@users.noreply.github.com>
2023-04-12 11:46:27 +0000

    deploy: 9878f6df8a14e61907f22415f1fb4af9e24d2c91

---
+++ source-files.js
@@ -0,0 +1,9 @@
+var sourcesIndex = JSON.parse('{\
+"book":["",[],["book.rs"]],\
+"cli":["",[],["cli.rs"]],\
+"fchess":["",[],["bitboard.rs","bitwise.rs","board.rs","book.rs","common.rs","dumb7fill.rs","lib.rs","move_generator.rs","moves.rs","moveset.rs","piece.rs","side.rs","solver.rs","square.rs"]],\
+"fen_diagram":["",[],["fen_diagram.rs"]],\
+"test_move_generation":["",[],["test_move_generation.rs"]],\
+"uci":["",[],["uci.rs"]]\
+}');
+createSourceSidebar();
