f061ffb1eba265d6734a40ae1a70790cd3224c85
João Freitas <joaj.freitas@gmail.com>
2025-06-02 18:27:03 +0100

    Refactor epd

--- src/epd.rs
+++ src/epd.rs
@@ -1,7 +1,6 @@
 use crate::Board;
 use anyhow::Result;
 use std::collections::HashMap;
-use std::collections::VecDeque;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Epd {
@@ -26,39 +25,25 @@ impl Epd {
     }
 
     pub fn from_string(s: &str) -> Result<Epd> {
-        let sp = s.split(" ").collect::<Vec<&str>>();
-        let fen = sp[..6]
-            .iter()
-            .map(|x| x.to_string())
-            .intersperse(" ".to_string())
-            .collect::<String>();
+        let mut properties: HashMap<String, String> = HashMap::default();
+        let mut iter = s.split(" ").into_iter().skip(6);
+        loop {
+            let key = iter.next();
+            let value = iter.next();
 
-        let mut properties = sp[6..]
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<VecDeque<String>>();
-
-        let mut property_map: HashMap<String, String> = HashMap::default();
-
-        let mut key = "".to_string();
-        let mut value;
-        let mut state = 0;
-
-        while !properties.is_empty() {
-            let v = properties.pop_front().unwrap();
-            if state == 0 {
-                key = v;
-                state = 1;
-            } else if state == 1 {
-                value = v;
-                state = 0;
-                property_map.insert(key.clone(), value.to_string());
+            if key.is_none() || value.is_none() {
+                break;
             }
+
+            properties.insert(
+                key.unwrap().to_string(),
+                value.unwrap().to_string(),
+            );
         }
 
         Ok(Epd {
-            board: Board::from_fen(&fen)?,
-            properties: property_map,
+            board: Board::from_fen(s)?,
+            properties,
         })
     }
 }
@@ -76,7 +61,7 @@ mod tests {
         assert_eq!(
             Epd::new(
                 &Board::from_fen(
-                    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 bm g6g4"
+                    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
                 )
                 .unwrap(),
                 HashMap::from([("bm".to_string(), "g6g4".to_string())])
