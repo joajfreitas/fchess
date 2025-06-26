0bebfb95e6edc47dce86b8f70235ad6c7aaccdee
João Freitas <joaj.freitas@gmail.com>
2025-06-26 20:57:27 +0100

    Allow human player to play on any side

--- bin/cli.rs
+++ bin/cli.rs
@@ -14,6 +14,38 @@ struct Args {
     /// path to polyglot openning book
     #[arg(short, long)]
     book: Option<String>,
+
+    #[arg(short, long)]
+    side: Option<String>,
+}
+
+fn read_line(rl: &mut rustyline::DefaultEditor) -> Result<String> {
+    let line = rl.readline("> ");
+    match line {
+        Ok(line) => {
+            rl.add_history_entry(line.as_str())?;
+            rl.save_history(".fchess_history").unwrap();
+            Ok(line)
+        }
+        Err(err) => {
+            println!("Error: {err:?}");
+            Err(anyhow!(err))
+        }
+    }
+}
+
+fn get_best_move(board: &Board, book: &Option<Book>, solver: &mut Solver) -> Result<String> {
+    Ok(match book.as_ref().and_then(|b| b.get_best_move(board)) {
+        Some(mov) => {
+            println!("=> Book move");
+            mov
+        }
+        _ => {
+            println!("=> Search move");
+            solver.best_move(board).unwrap()
+        }
+    }
+    .to_algebraic())
 }
 
 fn main() -> Result<()> {
@@ -25,6 +57,17 @@ fn main() -> Result<()> {
 
     let args = Args::parse();
 
+    let side = match args
+        .side
+        .unwrap_or("white".to_string())
+        .to_lowercase()
+        .as_str()
+    {
+        "white" => Side::White,
+        "black" => Side::Black,
+        side => panic!("Error: unexpected side argument: {}", side),
+    };
+
     let book = args.book.map(|book| Book::from_filename(&book));
 
     let mut board = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0")?;
@@ -32,34 +75,12 @@ fn main() -> Result<()> {
     println!("{board}");
 
     loop {
-        let mov: Move = Move::from_algebraic(&match board.get_turn() {
-            Side::White => {
-                let line = rl.readline("> ");
-                match line {
-                    Ok(line) => {
-                        rl.add_history_entry(line.as_str())?;
-                        rl.save_history(".fchess_history").unwrap();
-                        line
-                    }
-                    Err(err) => {
-                        println!("Error: {err:?}");
-                        return Err(anyhow!(err));
-                    }
-                }
-            }
-            Side::Black => match book.as_ref().and_then(|b| b.get_best_move(&board)) {
-                Some(mov) => {
-                    println!("=> Book move");
-                    mov
-                }
-                _ => {
-                    println!("=> Search move");
-                    solver.best_move(&board).unwrap()
-                }
-            }
-            .to_algebraic(),
-        })
-        .unwrap();
+        let algebra = &if board.get_turn() == side {
+            read_line(&mut rl)
+        } else {
+            get_best_move(&board, &book, &mut solver)
+        }?;
+        let mov: Move = Move::from_san(algebra, &board).unwrap();
 
         board = match board.apply(&mov) {
             Some(board) => board,
