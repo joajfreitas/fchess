f955fef127a79d05a1110df0b0f2b93878887ade
João Freitas <joaj.freitas@gmail.com>
2026-09-08 22:41:08 +0100

    Remove apply move and movegen binaries

--- scripts/movegen_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo run --release --bin test_movegen -- testcases/movegen/standard.yaml
-cargo run --release --bin test_movegen -- testcases/movegen/promotion.yaml
-cargo run --release --bin test_movegen -- testcases/movegen/enpassant.yaml
