d10f040881ad2b656691aeadc2d81c4150c1d7f8
João Freitas <joaj.freitas@gmail.com>
2026-09-11 23:39:34 +0100

    Strict check of generated moves equality

--- build/templates/rampart.rs
+++ build/templates/rampart.rs
@@ -1,19 +1,19 @@
 #[test]
 fn {name}() {{
+    let expected_moves = {expected_moves_san};
     let board = Board::from_fen("{starting_fen}").unwrap();
+    let mut expected_moves = expected_moves
+        .into_iter()
+        .map(|mov| Move::from_san(mov,&board).unwrap())
+        .collect::<Vec<Move>>();
     let move_generator = MoveGenerator::new();
 
     let moves = move_generator.generate_moves(&board);
 
-    let expected_mov = Move::from_san("{san}", &board).unwrap();
+    let mut moves = moves.into_iter().flat_map(|moveset| moveset.into_iter().collect::<Vec<Move>>()).collect::<Vec<Move>>();
 
-    let mut found_move = false;
-    for mov in moves {{
-        if mov.contains(&expected_mov){{
-            found_move = true;
-            break;
-        }}
-    }}
+    moves.sort();
+    expected_moves.sort();
 
-    assert!(found_move, "Expected move not found in generated moves");
+    assert_that!(moves, eq(&expected_moves));
 }}
